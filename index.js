b6bddbb2348cf2e31849b97b9ad35f3d435a6a97
ryan wong <ryanpersonal18@gmail.com>
2024-11-09 22:10:07 -0700

    Work

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import 'dotenv/config'
+import express from 'express';
+import { searchPrompt } from './util/searchPrompt.js';
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
 // TODO: Replace the following with your app's Firebase project configuration
+
+const app = express();
+const PORT = 3243;
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
@@ -16,8 +21,8 @@ const firebaseConfig = {
   
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const appDb = initializeApp(firebaseConfig);
+const db = getFirestore(appDb);
 
 async function getItems (db) {
     const itemsCol = collection(db, 'items');
@@ -30,3 +35,20 @@ async function getItems (db) {
 
 let data = await getItems(db);
 console.log(data);
+
+app.post("/searchPrompt", (req,res) => {
+    const text = req.body.text;
+
+
+})
+
+let text = `Like a garden in early spring - there's still some frost on the ground, but I can feel things starting to grow again. The medication is finally working, I think. Yesterday I caught myself making plans for next month, which I haven't done in... I can't even remember how long.`
+
+let llm = await searchPrompt(text);
+
+console.log(llm)
+
+
+app.listen(PORT, () => {
+    console.log("listening on port " + PORT)
+})
